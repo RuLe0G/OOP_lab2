950d51b626cbecde92f9060faedd6430bae2266d
RuLe0G <khlebnikov2015@yandex.ru>
2020-02-15 09:37:44 +0700

    1

---
+++ Oop_lab2/Oop_lab2/Point2D.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop_lab2
+{
+    class Point2D
+    {
+        private double x;
+        private double y;
+
+
+        public Point2D(){
+
+        }
+
+        public double GetX()
+        {
+            return 0;
+        }
+        public double getX()
+        {
+            return 0;
+        }
+
+        public double ShiftX()
+        {
+            return 0;
+        }
+
+        public double ShiftY()
+        {
+            return 0;
+        }
+
+        public double GetDistance()
+        {
+            return 0;
+        }
+    }
+
+
+}
